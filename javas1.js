70b1ebdbaeaf9e02d16fdf60102c8c9d4ab8e875
Prathamesh Chandekar <prathameshchandekar@Prathameshs-MacBook-Pro.local>
2023-03-30 11:54:25 +0530

    running a node js file

---
+++ javas1.js
@@ -0,0 +1,23 @@
+var a = 12;
+var b = 10;
+
+
+console.log("Hello world !!!");
+
+console.log("a : " + a  + "  &  b : " + b);
+
+console.log("Addition : " + (a+b));
+
+console.log("Subtraction : " + (a-b));
+
+
+
+// ****************
+
+// Output ->
+
+// ➜  Node_Basics git:(main) ✗ node javas1.js           
+// Hello world !!!
+// a : 12  &  b : 10
+// Addition : 22
+// Subtraction : 2
